25d9cab828be2e5875bc26b8d123b12592034920
Jaydon <pearcejaydon@gmail.com>
2022-07-29 14:53:25 +1000

    Fix password checking logic and fix re-sizing issue

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 const password = document.querySelectorAll('.pwd');
 
-password.forEach(pwd => pwd.addEventListener('input', testInput));
+password.forEach(pwd => pwd.addEventListener('focusout', testInput));
 
-const errorMsg = document.createElement('p');
-errorMsg.classList.add('error-msg');
-errorMsg.innerText = "The passwords do not match";
+const errorMsg = document.querySelector('.password-error');
+
 
 
 function testInput(){
-    console.log(password[0].value);
     if(password[0].value !== password[1].value){
-        //if(password[0].contains(errorMsg)) return;
-        password[0].appendChild(errorMsg);
+        if(password[0].contains(errorMsg)) return;
+        errorMsg.style.display = "inline";
         password[0].classList.add("error");
     }else{
         if(password[0].classList.contains("error")){
             password[0].classList.remove("error");
-            
+            errorMsg.style.display = "none";
         }
     }
 }
